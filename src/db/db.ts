eaf3f7039d11b3e911b0e30a7217345314a60e7b
fevenissayas <feven.issayas-ug@aau.edu.et>
2025-04-10 11:20:17 +0300

    supabase connection

--- src/db/db.ts
+++ src/db/db.ts
@@ -2,8 +2,13 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
 
-const queryClient = postgres(process.env.DATABASE_URL!, { 
-  ssl: process.env.NODE_ENV === 'production' ? "require" : false 
+const databaseUrl =
+  process.env.NODE_ENV === 'production'
+    ? process.env.DIRECT_URL
+    : process.env.DATABASE_URL;
+    
+const queryClient = postgres(databaseUrl!, {
+  ssl: true,
 });
 
 export const db = drizzle(queryClient, { schema });
