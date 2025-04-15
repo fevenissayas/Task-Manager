0624408279244d608f878d537833543495dfe37c
fevenissayas <feven.issayas-ug@aau.edu.et>
2025-04-15 11:47:07 +0300

    server side rendering

--- src/app/edit_task/[id]/page.tsx
+++ src/app/edit_task/[id]/page.tsx
@@ -2,15 +2,16 @@ import { Metadata } from "next";
 import UpdateTaskPage from "@/components/UpdateTaskPage";
 
 interface EditTaskPageProps {
-  params: {
+  params: Promise<{
     id: string;
-  };
+  }>;
 }
 
 export const metadata: Metadata = {
   title: "Edit Task",
 };
 
-export default function Page({ params }: EditTaskPageProps) {
-  return <UpdateTaskPage taskId={params.id} />;
+export default async function Page({ params }: EditTaskPageProps) {
+  const resolvedParams = await params;
+  return <UpdateTaskPage taskId={resolvedParams.id} />;
 }
--- src/components/UpdateTaskPage.tsx
+++ src/components/UpdateTaskPage.tsx
@@ -1,4 +1,3 @@
-// components/UpdateTaskPage.tsx
 "use client";
 
 import { useRouter } from "next/navigation";
